bc507c7a96c6a430a080117c3d61099b9c9f0e75
Ogarkov Sergey <coriouse@gmail.com>
2018-07-27 11:51:48 +0300

    PlusOne

---
+++ src/app/PlusOne.java
@@ -0,0 +1,37 @@
+package app;
+
+import java.util.Arrays;
+
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * <p>
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ * <p>
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ */
+public class PlusOne {
+
+
+    public static void main(String[] args) {
+        //int[] digits = {1,2,3};
+        //int[] digits = {4,3,2,1};
+        int[] digits = {10};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
